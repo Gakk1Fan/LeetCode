39b9e5f39e24875dce32b8d8ba12e40ddeb51039
Gakk1Fan <yinyh@pku.edu.cn>
2022-05-27 17:46:12 +0800

    test

--- test01.py
+++ test01.py
@@ -56,17 +56,6 @@ def List2Tree(l: List) -> TreeNode:
                 l.pop(0)
     return root
 
-class Solution:
-    def findSubstringInWraproundString(self, p: str) -> int:
-        dp, cur = [0] * 26, 1
-        dp[ord(p[0]) - ord('a')] = 1
-        for c1, c2 in pairwise(p):
-            if not (ord(c2) - ord(c1) - 1) % 26:
-                cur += 1
-            else:
-                cur = 1
-            dp[idx] = max(dp[idx := ord(c2) - ord('a')], cur)
-        return sum(dp)
 
 
 if __name__ == "__main__":
